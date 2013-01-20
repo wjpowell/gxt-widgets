2c839dbaa910fb2b2a7843435154cd482476838e
William Powell <williamjpowell@gmail.com>
2013-01-19 20:22:17 -0500

    Modified the grid for watir
    The grid now works in Watir.  I had to override the
    include_platform_for method to use an extended WatirWebDriver::Table.

--- lib/gxt-widgets/gxt_grid.rb
+++ lib/gxt-widgets/gxt_grid.rb
@@ -1,3 +1,5 @@
+require 'watir-webdriver'
+require 'page-object/platforms/watir_webdriver/table'
 class GxtTable < PageObject::Elements::Table
 
   PageObject.register_widget :gxt_table, GxtTable, 'div'
@@ -6,4 +8,28 @@ class GxtTable < PageObject::Elements::Table
   def child_xpath
     ".//descendant::tr"
   end
+
+  def include_platform_for platform
+    super
+    if platform[:platform] == :watir_webdriver
+      require 'page-object/platforms/watir_webdriver/table'
+      self.class.send :include, WatirPageObjectGxtTable
+    elsif platform[:platform] == :selenium_webdriver
+      require 'page-object/platforms/selenium_webdriver/table'
+      self.class.send :include, PageObject::Platforms::SeleniumWebDriver::Table
+    else
+      raise ArgumentError, "expect platform to be :watir_webdriver or :selenium_webdriver"
+    end
+  end
+end
+
+module WatirPageObjectGxtTable
+  include PageObject::Platforms::WatirWebDriver::Table
+
+  def [](idx)
+    idx = find_index_by_title(idx) if idx.kind_of?(String)
+    return nil unless idx
+    Object::PageObject::Elements::TableRow.new(element.trs(:xpath => child_xpath)[idx], :platform => :watir_webdriver)
+  end
+
 end
