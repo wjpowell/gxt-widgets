05fae4eaf607ea7b74f1bdea602c48662dcf1d6f
William Powell <williamjpowell@gmail.com>
2013-03-13 21:26:16 -0400

    refactoring for grouping.  Seems to have created a problem.

--- lib/gxt-widgets/platforms/selenium_webdriver/gxt_grid.rb
+++ lib/gxt-widgets/platforms/selenium_webdriver/gxt_grid.rb
@@ -3,15 +3,8 @@ require 'gxt-widgets/gxt_grid_header_row'
 module GxtWidgets
   module Platforms
     module SeleniumWebDriver
-      module GxtGrid
-        include PageObject::Platforms::SeleniumWebDriver::Table
-
-        def [](idx)
-          eles = table_rows
-          idx = find_index_by_title(idx, eles) if idx.kind_of?(String)
-          return nil unless idx
-          initialize_row(eles[idx], :platform => :selenium_webdriver)
-        end
+      module GxtHeaderGrid
+        include Gxtwidgets::Platforms::SeleniumWebDriver::RowContainer
 
         def header
           initialize_header(table_rows[0], :platform => :selenium_webdriver)
--- lib/gxt-widgets/platforms/watir_webdriver/gxt_grid.rb
+++ lib/gxt-widgets/platforms/watir_webdriver/gxt_grid.rb
@@ -4,28 +4,10 @@ module GxtWidgets
   module Platforms
     module WatirWebDriver
       module GxtGrid
-        include PageObject::Platforms::WatirWebDriver::Table
-
-        def [](idx)
-          idx = find_index_by_title(idx) if idx.kind_of?(String)
-          return nil unless idx
-          initialize_row(row_collection[idx], :platform => :watir_webdriver)
-        end
+        include GxtWidgets::Platforms::WatirWebDriver::RowContainer
 
         def header
           initialize_header(row_collection[0], :platform => :watir_webdriver)
-        end
-
-        private
-
-        def find_index_by_title(row_title)
-          row_collection.find_index do |row|
-            row.cells.any? { |col| col.text.include? row_title }
-          end
-        end
-
-        def row_collection
-          element.trs(:xpath => child_xpath)
         end
       end
     end
